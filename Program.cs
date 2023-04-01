8c57448cbbf436b145a3ff72c7a325b26876db98
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-04-01 16:03:08 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// Calcule a área de um triângulo qualquer, dadas as medidas dos 3 lados. Exiba o semiperímetro e a área.
+
+double a;
+double b;
+double c;
+double p; //semiperimetro
+double area;
+
+Console.WriteLine("----Calculadora de Triângulo----\n");
+
+Console.Write("Digite a medida do lado a: ");
+a = double.Parse(Console.ReadLine()!);
+
+Console.Write("Digite a medida do lado b: ");
+b = double.Parse(Console.ReadLine()!);
+
+Console.Write("Digite a medida do lado a: ");
+c = double.Parse(Console.ReadLine()!);
+
+p = a + b + c / 2;
+area = Math.Sqrt (p * (p - a) * (p -b) * (p- c));
+
+Console.WriteLine($"o semiperímetro é {p}");
+Console.WriteLine($"A área do triângulo é {area}");
+Console.ReadLine();
